0a271658655095bd50fb319aabcd5c84dd3356a8
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-17 14:52:30 +0545

    feat: implementation of pagination in coin list

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -21,7 +21,12 @@ const Coins = () => {
     const [page, setPage] = useState(1);
     const [currency, setCurrency] = useState("usd")
     const currencySymbol = currency === "cad" ? "c$" : currency === "eur" ? "€" : "$";
+    const btns = new Array(132).fill(1);
 
+    const changePage = (page) => {
+        setPage(page);
+        setLoading(true);
+    };
     useEffect(() => {
         const fetchCoins = async () => {
             try {
@@ -47,6 +52,18 @@ const Coins = () => {
                                 name={i.name} img={i.image}
                                 price={i.current_price} symbol={i.symbol}
                                 url={i.url} currencySymbol={currencySymbol} />
+                        ))
+                    }
+                </HStack>
+                <HStack w={"full"} overflowX={"auto"} p={"8"}>
+                    {
+                        btns.map((item, index) => (
+                            <Button
+                                bgColor={"blackAlpha.900"}
+                                color={"white"}
+                                onClick={() => changePage(index + 1)}>
+                                {index + 1}
+                            </Button>
                         ))
                     }
                 </HStack>
