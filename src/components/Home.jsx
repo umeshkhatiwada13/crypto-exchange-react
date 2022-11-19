1dd0385548f66c5be30e32e4c41c27ece81b67c5
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-19 23:05:13 +0545

    worjing on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+    return 
+        <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"} >
+            </Box>
 }
 
 export default Home
