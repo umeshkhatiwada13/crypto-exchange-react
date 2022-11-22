009f910d3bc4c8b0ced90dca185d3fbed4a599e2
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-22 11:27:06 +0545

    feat: addition of animation in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,29 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react';
 import btcImage from '../assets/btc.png';
-
+import { motion } from "framer-motion";
 const Home = () => {
     return (
         <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"} >
-            <Image w={"full"} h={"full"} objectFit={"contain"} src={btcImage}></Image>
+            <motion.div style={{
+                height: "80vh",
+            }}
+                animate={{
+                    translateY: "20px"
+                }}
+                transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                }}>
+                <Image w={"full"} h={"full"} objectFit={"contain"} src={btcImage}></Image>
+            </motion.div>
             <Text
                 fontSize={"6xl"}
                 textAlign={"center"}
                 fontWeight={"thin"}
                 color={"whiteAlpha.700"}
-                mt={"-20"}
+                mt={"-10"}
             >My Crypto App </Text>
         </Box>
     )
